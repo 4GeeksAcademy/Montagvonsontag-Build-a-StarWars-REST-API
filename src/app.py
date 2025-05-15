ee5a79e2b57e0d94bc258328f169d483defb8cae
Montagvonsontag <37384660+Montagvonsontag@users.noreply.github.com>
2025-05-15 19:13:28 +0000

    First commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Favourite
+from sqlalchemy import select
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +37,163 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def get_users():
+    # esto se puede poner dentro del execute() sin problemas
+    stmt = select(User) 
+    # devuelve una lista con todos los registros de la tabla, cada uno como un <Objeto>
+    users = db.session.execute(stmt).scalars().all() 
+    # el scalars() se ocupa de devolverlos como objeto, sino seran devueltos como tuplas.
+    # como tupla NO pueden ejecutar el serialize
+    print('users sin serialize',users) #users sin serialize [<User 1>, <User 2>]
+    print('users despues de loop con serialize', [user.serialize() for user in users])
+    #users despues de loop con serialize 
+    # [
+    # {'id': 1, 'email': 'alice@example.com', 'profile': {'id': 1, 'bio': 'Soy Alice'}}, 
+    # {'id': 2, 'email': 'bob@example.com', 'profile': {'id': 2, 'bio': 'Soy Bob'}}
+    #]
+    #siempre que se nos devuelva una lista/coleccion, hay que serializar dentro de un loop
+    return jsonify([user.serialize() for user in users]), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+# GET: Get one user by id
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    stmt = select(User).where(User.user_id == user_id)
+    user = db.session.execute(stmt).scalar_one_or_none()
+    if user is None:
+        return jsonify({"error": "User not found"}), 404
+    return jsonify(user.serialize()), 200
 
-    return jsonify(response_body), 200
+# POST: Create new user
+@app.route("/users", methods=["POST"])
+def create_user():
+    #extraemos la informacion del body puede ser con request.json
+    data = request.get_json()
+    #verificamos que tenemos los elementos OBLIGATORIOS para crear un registro nuevo
+    if not data or "user_name" not in data or "first_name" not in data or "last_name" not in data or "email" not in data or "password" not in data:
+        return jsonify({"error": "Missing data"}), 400
+    
+    new_user = User(
+        user_name=data["user_name"],
+        first_name=data["first_name"],
+        last_name=data["last_name"],
+        email=data["email"],
+        password=data["password"],  # To be hashed (in production)
+    )
+    #Addition to DB
+    db.session.add(new_user)
+    #storage of data
+    db.session.commit()
+    return jsonify(new_user.serialize()), 201
+   
+# DELETE USER 
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    stmt = select(User).where(User.user_id == user_id)
+    user = db.session.execute(stmt).scalar_one_or_none()
+    if user is None:
+        return jsonify({'error':'User not found'}), 404
+    if user.favourites:
+        db.session.delete(user.favourites)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message':'user deleted'}),200
+
+# UPDATE USER
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    data = request.get_json()
+    stmt = select(User).where(User.user_id == user_id)
+    user = db.session.execute(stmt).scalar_one_or_none()
+    if user is None:
+        return jsonify({'error':'User not found'}), 404
+    if "email" in data:
+        user.email=data["email"]
+    if "user_name" in data:
+        user.user_name=data["user_name"]
+    if "first_name" in data:
+        user.first_name=data["first_name"]
+    if "last_name" in data:
+        user.last_name=data["last_name"]
+    if "password" in data:
+        user.password=data["password"]
+    #Storage of data
+    db.session.commit()
+    return jsonify(user.serialize()),200
+
+@app.route('/characters', methods=['GET'])
+def get_characters():
+    stmt = select(Character) 
+    characters = db.session.execute(stmt).scalars().all() 
+    return jsonify([character.serialize() for character in characters]), 200
+
+@app.route("/characters", methods=["POST"])
+def create_character():
+    data = request.get_json()
+    if not data or "character_name" not in data:
+        return jsonify({"error": "Missing data"}), 400
+    new_character = Character(
+        character_name=data["character_name"],
+    )
+    #Addition to DB
+    db.session.add(new_character)
+    #Storage of data
+    db.session.commit()
+    return jsonify(new_character.serialize()), 201
+
+# DELETE Character 
+@app.route('/characters/<int:id_character>', methods=['DELETE'])
+def delete_character(id_character):
+    stmt = select(Character).where(Character.id_character == id_character)
+    character = db.session.execute(stmt).scalar_one_or_none()
+    if character is None:
+        return jsonify({'error':'Character not found'}), 404
+    if character.favourites:
+        db.session.delete(character.favourites)
+    db.session.delete(character)
+    db.session.commit()
+    return jsonify({'message':'character deleted'}),200
+
+@app.route("/favourites", methods=["GET"])
+def get_favourites():
+    stmt = select(Favourite) 
+    favourites = db.session.execute(stmt).scalars().all() 
+    return jsonify([favourite.serialize() for favourite in favourites]), 200
+
+#POST: Favourites
+@app.route('/favourites/<int:user_id>/<int:id_character>', methods=['POST'])
+def add_favourite(user_id, id_character):
+    user = db.session.get(User, user_id)
+    character = db.session.get(Character, id_character)
+    if not user:
+        return jsonify({"error": "User does not exist"}), 404
+    if not character:
+        return jsonify({"error": "Character does not exist"}), 404
+
+    #Check for duplicate favourite  
+    existing = db.session.execute(
+        select(Favourite).where(Favourite.user_id==user_id, Favourite.id_character==id_character)
+    ).scalar_one_or_none()
+    if existing:
+        return jsonify({"error": "Favourite already exists"}), 409
+    new_favourite = Favourite(
+        user_id=user_id,
+        id_character=id_character
+    )
+    db.session.add(new_favourite)
+    db.session.commit() 
+    return jsonify(new_favourite.serialize()), 201
+
+# DELETE FAVOURITE
+@app.route('/favourites/<int:user_id>/<int:id_character>', methods=['DELETE'])
+def delete_favourite(user_id, id_character):
+    stmt = select(Favourite).where(Favourite.user_id == user_id and Favourite.id_character == id_character)
+    favourite = db.session.execute(stmt).scalar_one_or_none()
+    if favourite is None:
+        return jsonify({'error':'favourite not found'}), 404
+    db.session.delete(favourite)
+    db.session.commit()
+    return jsonify({'message':'favourite deleted'}),200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,78 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    __tablename__ = "users"
+    #the below attributes are shown up in admin/User
+    user_id: Mapped[int] = mapped_column(primary_key=True)
+    user_name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(50),nullable=False)
+    
+    #Relationships with other tables: 
+    favourites: Mapped[list["Favourite"]] = relationship("Favourite", back_populates="user") #one to many relationship
+    
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "user_name": self.user_name,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "favourites": [
+                {
+                    "id_character": fav.character.id_character,
+                    "character_name": fav.character.character_name
+                } for fav in self.favourites if fav.character
+            ]
+            # do not serialize the password, its a security breach
+        }
+    
+class Character(db.Model):
+    __tablename__ = "characters"
+    id_character: Mapped[int] = mapped_column(primary_key=True)
+    character_name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+      
+    # Relationships with other tables
+    favourites: Mapped[list["Favourite"]] = relationship("Favourite", back_populates="character")
 
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+            "id_character": self.id_character,
+            "character_name": self.character_name,
+            "favourite_character_of": [
+                {
+                    "user_id": fav.user.user_id,
+                    "user_name": fav.user.user_name
+                } for fav in self.favourites if fav.user
+            ]    
+        }     
+    
+#Association Table
+class Favourite(db.Model):
+    __tablename__ = "favourites"
+        
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.user_id"), primary_key=True,nullable=False)
+    id_character: Mapped[int] = mapped_column(ForeignKey("characters.id_character"), primary_key=True, nullable=False)
+    
+    #Relationships with other tables
+    user: Mapped["User"] = relationship("User",back_populates="favourites")
+    character: Mapped["Character"] = relationship("Character", back_populates="favourites")
+
+    def serialize(self):
+        return {
+            
+            "user": {
+                "user_id": self.user_id               
+            } if self.user else None,    
+            "character": {
+                "id_character": self.id_character                
+            } if self.character else None,           
+        }    
+    
+    
